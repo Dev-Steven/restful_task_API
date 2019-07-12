ebb97a9314d97c5518c70a075084028c1fcf041b
Steven <steven.chung96@yahoo.com>
2019-07-12 12:11:32 -0700

    added dist folder

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static( __dirname + '/public/dist/public' ));
 
 // Mongoose
 require('./server/config/mongoose.js');
